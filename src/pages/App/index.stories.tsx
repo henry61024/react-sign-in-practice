229a01ce0d25547dc2ebe1b542ebf05faae2cac0
Henry Chang <henry35208@hotmail.com.tw>
2020-02-28 14:42:30 +0800

    refactor(folder): add components/pages/assets folders

---
+++ src/pages/App/index.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './index';
+import '../../index.css';
+
+export default {
+  title: 'Pages/App',
+  component: App,
+};
+
+export const Page = () => <App></App>;
