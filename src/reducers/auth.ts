0b5cf7ed3774a43c31da33d40637b07372f0c9c2
Henry Chang <henry35208@hotmail.com.tw>
2020-03-16 11:05:40 +0800

    fix(authReducer): make auth reducer check stricker

--- src/reducers/auth.ts
+++ src/reducers/auth.ts
@@ -18,14 +18,14 @@ import {
   SIGN_OUT_FAIL_STATUS,
 } from '../types';
 
-const initAuthState: AuthState = {
+const initialState: AuthState = {
   status: SIGN_IN_INITIAL_STATUS,
   username: null,
   password: null,
 };
 
 const auth = (
-  state: AuthState = initAuthState,
+  state: AuthState = initialState,
   action: AuthActions
 ): AuthState => {
   switch (action.type) {
@@ -36,10 +36,14 @@ const auth = (
         password: action.password,
       };
     case SIGN_IN_SUCCESS:
+      const isStateAccountLegal = !!(state.username && state.password);
       return {
-        status: SIGN_IN_SUCCESS_STATUS,
+        status: isStateAccountLegal
+          ? SIGN_IN_SUCCESS_STATUS
+          : SIGN_IN_FAIL_STATUS,
         username: state.username,
         password: state.password,
+        error: isStateAccountLegal ? undefined : 'Illegal account',
       };
     case SIGN_IN_FAIL:
       return {
@@ -51,8 +55,8 @@ const auth = (
     case SIGN_IN_CANCEL:
       return {
         status: SIGN_IN_CANCEL_STATUS,
-        username: null,
-        password: null,
+        username: state.username,
+        password: state.password,
       };
     case SIGN_OUT_REQUEST:
       return {
@@ -71,6 +75,7 @@ const auth = (
         status: SIGN_OUT_FAIL_STATUS,
         username: state.username,
         password: state.password,
+        error: action.error,
       };
     default:
       return state;
